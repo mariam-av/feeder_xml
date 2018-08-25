f1bf95444ce8794d849daea97dab0df8d9687ec3
mariam-av <avemariam20@gmail.com>
2018-08-25 15:25:58 +0400

    keyword xml parser

---
+++ src/main/java/rules/Keyword.java
@@ -0,0 +1,30 @@
+package rules;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlValue;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Keyword {
+    @XmlValue
+    protected String value;
+	@XmlAttribute
+	private String wordMatch;
+
+	public String getWordMatch() {
+		return wordMatch;
+	}
+
+	public void setWordMatch(String wordMatch) {
+		this.wordMatch = wordMatch;
+	}
+
+	@Override
+	public String toString() {
+		return "wordMatch = " + wordMatch + ", value = " + value;
+	}
+
+}
