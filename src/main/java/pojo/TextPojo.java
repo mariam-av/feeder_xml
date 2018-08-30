185c8b1cb36213caeaecdb51ec4675e1eead92ac
mariam-av <avemariam20@gmail.com>
2018-08-30 23:30:23 +0400

    TextPojo changed

--- src/main/java/pojo/TextPojo.java
+++ src/main/java/pojo/TextPojo.java
@@ -6,64 +6,85 @@ public class TextPojo {
 	private String body;
 	private Boolean matchInBody;
 	private Boolean matchInTitle;
+	private Boolean matchInSummary;
 	private Boolean matched;
 	private Integer matchesInBody;
 	private Integer matchesInTitle;
-
+	private Integer matchesInSummary;
+	private Integer matchIndexInBody;
+	private Integer matchIndexInTitle;
+	private Integer matchIndexInSummary;
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 	public String getBody() {
 		return body;
 	}
-
 	public void setBody(String body) {
 		this.body = body;
 	}
-
 	public Boolean getMatchInBody() {
 		return matchInBody;
 	}
-
 	public void setMatchInBody(Boolean matchInBody) {
 		this.matchInBody = matchInBody;
 	}
-
 	public Boolean getMatchInTitle() {
 		return matchInTitle;
 	}
-
 	public void setMatchInTitle(Boolean matchInTitle) {
 		this.matchInTitle = matchInTitle;
 	}
-
+	public Boolean getMatchInSummary() {
+		return matchInSummary;
+	}
+	public void setMatchInSummary(Boolean matchInSummary) {
+		this.matchInSummary = matchInSummary;
+	}
 	public Boolean getMatched() {
 		return matched;
 	}
-
 	public void setMatched(Boolean matched) {
 		this.matched = matched;
 	}
-
 	public Integer getMatchesInBody() {
 		return matchesInBody;
 	}
-
 	public void setMatchesInBody(Integer matchesInBody) {
 		this.matchesInBody = matchesInBody;
 	}
-
 	public Integer getMatchesInTitle() {
 		return matchesInTitle;
 	}
-
 	public void setMatchesInTitle(Integer matchesInTitle) {
 		this.matchesInTitle = matchesInTitle;
+	}
+	public Integer getMatchesInSummary() {
+		return matchesInSummary;
+	}
+	public void setMatchesInSummary(Integer matchesInSummary) {
+		this.matchesInSummary = matchesInSummary;
+	}
+	public Integer getMatchIndexInBody() {
+		return matchIndexInBody;
+	}
+	public void setMatchIndexInBody(Integer matchIndexInBody) {
+		this.matchIndexInBody = matchIndexInBody;
+	}
+	public Integer getMatchIndexInTitle() {
+		return matchIndexInTitle;
+	}
+	public void setMatchIndexInTitle(Integer matchIndexInTitle) {
+		this.matchIndexInTitle = matchIndexInTitle;
+	}
+	public Integer getMatchIndexInSummary() {
+		return matchIndexInSummary;
+	}
+	public void setMatchIndexInSummary(Integer matchIndexInSummary) {
+		this.matchIndexInSummary = matchIndexInSummary;
 	}
 
 }
